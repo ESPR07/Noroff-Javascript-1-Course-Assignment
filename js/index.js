0e7c52e5aea1663b74068277896c9576b0a978ff
ESPR07 <111272036+ESPR07@users.noreply.github.com>
2023-02-06 13:06:09 +0100

    Additions + Styling
    Added some more styling and redone insertion of HTML to make it more secure.

--- js/index.js
+++ js/index.js
@@ -10,20 +10,50 @@ async function callAPI(){
 
     for(i = 0; i < apiArray.length; i++){
         const upperCaseName = apiArray[i].name.toUpperCase();
-        const apostropheLessImageURL = apiArray[i].image.replace("'", "%27");
+        const apostropheLessImageURL = apiArray[i].image.replace("'", "%27"); //replaces Apostrophe from URL with compatible code.
         
-        if(i === 12){
+        if(i === 15){
             break;
         }
 
-        container.innerHTML += `<a class="itemContainer" href="/details.html?id=${apiArray[i].id}">
-                                    <h3 class="itemHeader">${upperCaseName}</h3>
-                                    <div class="itemImage" style="background-image: url(${apostropheLessImageURL})"></div>
-                                    <p>Attack Power: ${apiArray[i].attack}</p>
-                                    <p>Defense: ${apiArray[i].defense}</p>
-                                </a>`
+        // container.innerHTML += `<a class="itemContainer" href="/details.html?id=${apiArray[i].id}">
+        //                             <h3 class="itemHeader">${upperCaseName}</h3>
+        //                             <div class="itemImage" style="background-image: url(${apostropheLessImageURL})"></div>
+        //                             <p>Attack Power: ${apiArray[i].attack}</p>
+        //                             <p>Defense: ${apiArray[i].defense}</p>
+        //                         </a>`
 
         
+        // createElement method of doing the same thing I did over:
+
+        function createHTML(){
+            let a = document.createElement('a');
+            a.classList.add('itemContainer');
+            a.href = "/details.html?id=" + apiArray[i].id;
+
+            let h3 = document.createElement('h3');
+            h3.classList.add("itemHeader");
+            h3.innerText = upperCaseName;
+
+            let htmlImage = document.createElement('div')
+            htmlImage.classList.add("itemImage");
+            htmlImage.style.cssText = "background-image: url(" + apostropheLessImageURL + ")"
+
+            let pElementAttack = document.createElement('p');
+            pElementAttack.innerText = "Attack Power: " + apiArray[i].attack;
+
+            let pElementDefense = document.createElement('p');
+            pElementDefense.innerText = "Defense: " + apiArray[i].defense;
+
+            container.appendChild(a);
+            a.appendChild(h3);
+            a.appendChild(htmlImage);
+            a.appendChild(pElementAttack);
+            a.appendChild(pElementDefense);
+        }
+
+        createHTML();
+
         
         console.log(apiArray[i]);
     }
